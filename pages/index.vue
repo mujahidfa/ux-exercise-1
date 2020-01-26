704b62988465f962f0d20831ddea7f927eedab58
Mujahid Anuar <mujahidfanuar@gmail.com>
2020-01-26 00:42:03 -0600

    format file

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="bg-top bg-blue-500 h-56 background">
-    <div class="p-6 min-h-screen tracking-widest">
+  <div class="h-56 bg-blue-500 bg-top background">
+    <div class="min-h-screen p-6 tracking-widest">
       <div class="">
         <div class="flex flex-row justify-between">
-          <img class="h-5 w-5" src="~/assets/left-arrow.svg" />
+          <img class="w-5 h-5" src="~/assets/left-arrow.svg" />
           <img src="~/assets/menu.svg" />
         </div>
-        <h1 class="text-white font-bold text-center mb-4 mt-8">Payment</h1>
+        <h1 class="mt-8 mb-4 font-bold text-center text-white">Payment</h1>
         <div
-          class="flex flex-col p-4 h-48 my-2 bg-red-400 rounded-lg content-around shadow-xl text-white font-semibold"
+          class="flex flex-col content-around h-48 p-4 my-2 font-semibold text-white bg-red-400 rounded-lg shadow-xl"
         >
           <div class="">
             <img src="~/assets/mcard.svg" />
           </div>
-          <div class="flex flex-row mt-3 items-center">
+          <div class="flex flex-row items-center mt-3">
             <div class="text-3xl dots">•••• •••• ••••</div>
-            <div class="ml-4 mt-1 numbers">1234</div>
+            <div class="mt-1 ml-4 numbers">1234</div>
           </div>
           <div class="flex flex-row justify-between mt-8">
             <div class="">Mujahid Fakhrul Anuar</div>
@@ -26,7 +26,7 @@
 
       <div class="margin">
         <h1 class="font-bold text-gray-800">Card number</h1>
-        <div class="py-4 px-5 bg-white rounded-lg shadow-lg text-gray-500">
+        <div class="px-5 py-4 text-gray-500 bg-white rounded-lg shadow-lg">
           0000 0000 0000 0000
         </div>
       </div>
@@ -36,7 +36,7 @@
           <div class="flex-grow ">
             <h1 class="font-bold text-gray-800">Expiry date</h1>
             <div
-              class="py-4 px-5 mr-4 bg-white rounded-lg shadow-lg text-gray-500"
+              class="px-5 py-4 mr-4 text-gray-500 bg-white rounded-lg shadow-lg"
             >
               MM/YY
             </div>
@@ -44,7 +44,7 @@
 
           <div>
             <h1 class="font-bold text-gray-800">Secure code</h1>
-            <div class="py-4 px-5 bg-white rounded-lg shadow-lg text-gray-500">
+            <div class="px-5 py-4 text-gray-500 bg-white rounded-lg shadow-lg">
               CVV
             </div>
           </div>
@@ -54,10 +54,10 @@
         <h1 class="font-bold text-gray-800">Secure code</h1>
       </div>
       <div class="flex flex-row text-gray-500">
-        <div class="py-4 px-5 mr-4 flex-grow bg-white rounded-lg shadow-lg ">
+        <div class="flex-grow px-5 py-4 mr-4 bg-white rounded-lg shadow-lg ">
           MM/YY
         </div>
-        <div class="py-4 px-5 flex-grow bg-white rounded-lg shadow-lg ">
+        <div class="flex-grow px-5 py-4 bg-white rounded-lg shadow-lg ">
           CVV
         </div>
       </div> -->
@@ -65,17 +65,17 @@
 
       <div class="margin">
         <h1 class="font-bold text-gray-800">Name on card</h1>
-        <div class="py-4 px-5 bg-white rounded-lg shadow-lg text-gray-500">
+        <div class="px-5 py-4 text-gray-500 bg-white rounded-lg shadow-lg">
           Mujahid Fakhrul Anuar
         </div>
       </div>
 
       <div
-        class="margin py-4 px-5 bg-red-400 rounded-lg shadow-lg text-white font-bold text-center"
+        class="px-5 py-4 font-bold text-center text-white bg-red-400 rounded-lg shadow-lg margin"
       >
         Pay
       </div>
-      <hr class="mt-8 border-gray-400 border-2 rounded mx-20 mt-12" />
+      <hr class="mx-20 mt-8 mt-12 border-2 border-gray-400 rounded" />
     </div>
   </div>
 </template>
